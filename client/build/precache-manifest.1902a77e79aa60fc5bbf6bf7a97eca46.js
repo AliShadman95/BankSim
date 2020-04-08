90a74c2a544136fbed753682733c238d4f9db887
Ali Shadman <ali.shadman@sourcesense.com>
2020-04-08 15:38:41 +0200

    com

---
+++ client/build/precache-manifest.1902a77e79aa60fc5bbf6bf7a97eca46.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f0d857b0dc04019b5af7128fe7c436",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfbffd7ca6a1b2b35d81",
+    "url": "/static/css/2.4cda0006.chunk.css"
+  },
+  {
+    "revision": "9c0019b69299c7d882b7",
+    "url": "/static/css/main.64410f1e.chunk.css"
+  },
+  {
+    "revision": "dfbffd7ca6a1b2b35d81",
+    "url": "/static/js/2.38d59644.chunk.js"
+  },
+  {
+    "revision": "422a2fe4c24d768ee3afbe0a75048afa",
+    "url": "/static/js/2.38d59644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c0019b69299c7d882b7",
+    "url": "/static/js/main.c6af153f.chunk.js"
+  },
+  {
+    "revision": "0cda4f2983d2d66038e9",
+    "url": "/static/js/runtime-main.d9e7cdb4.js"
+  },
+  {
+    "revision": "66ac7bf3f8a4daf6ef2f210c4138787d",
+    "url": "/static/media/bg-waves-1024.66ac7bf3.png"
+  },
+  {
+    "revision": "cd74b34ffcea722fb00f19f5290e2eaa",
+    "url": "/static/media/green-sphere-312.cd74b34f.png"
+  },
+  {
+    "revision": "d428f5996b42d34e77e244ae1edd4854",
+    "url": "/static/media/hero-bg-3840.d428f599.png"
+  },
+  {
+    "revision": "b9fb62f8cc1dd9efda48ba207080862d",
+    "url": "/static/media/logo_transparent.b9fb62f8.png"
+  },
+  {
+    "revision": "faf847d4f96db134c90d0b2487f2384b",
+    "url": "/static/media/microphone.faf847d4.png"
+  },
+  {
+    "revision": "c826fa2b0c5aafdb571afde855a64dc2",
+    "url": "/static/media/orange_sphere-312.c826fa2b.png"
+  },
+  {
+    "revision": "db6a4d101d02fac7aa8a25c795992ce4",
+    "url": "/static/media/purple-sphere-312.db6a4d10.png"
+  },
+  {
+    "revision": "b352b33387c3c4f9f85adec00265ec61",
+    "url": "/static/media/waves-slider-blue-6176.b352b333.png"
+  }
+]);
